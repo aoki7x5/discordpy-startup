47c0705e77cf39cad679dc4716ab4023ef2d2c96
aoki7x5 <79979909+aoki7x5@users.noreply.github.com>
2021-03-03 21:53:23 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,6 +1,8 @@
 from discord.ext import commands
 import os
 import traceback
+import asyncio 
+import random 
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
@@ -16,6 +18,18 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
+
+
+
+@bot.event
+async def on_message(message):
+    if message.content.startswith("!dice6"): #ここの!diceは好きなのにしていいぞ
+        if bot.user != message.author:
+            num_random = random.randrange(1,6)
+            m = str(num_random)
+            await bot.send_message(message.channel, m)
+
+bot.run("[手に入れたトークンを記入しよう]")
 
 
 bot.run(token)
